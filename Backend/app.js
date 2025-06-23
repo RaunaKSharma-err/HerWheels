3c600af62db74fc84dfe17c0402c80d0a4e4b646
RaunaKSharma-err <progritik@gmail.com>
2025-06-23 10:11:58 +0545

    final commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,33 +1,36 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const cookieParser = require('cookie-parser');
-const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes');
-const captainRoutes = require('./routes/captain.routes');
-const rideRoutes = require('./routes/ride.routes');
-
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
+const rideRoutes = require("./routes/ride.routes");
+const path = require("path");
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const _dirname = path.resolve();
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello and Welcome to Herwheels");
 });
 
-app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
-app.use('/rides', rideRoutes);
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+app.use("/rides", rideRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 module.exports = app;
-
