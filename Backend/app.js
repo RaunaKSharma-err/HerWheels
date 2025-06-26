1dbf32ad17d6c9d5a12df90324ef861b28ad0ff9
RaunaKSharma-err <progritik@gmail.com>
2025-06-26 20:24:12 +0545

    final commit

--- Backend/app.js
+++ Backend/app.js
@@ -32,5 +32,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 module.exports = app;
